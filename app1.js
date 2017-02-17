a212b94573662e0e786770a36571395b3a3e01d7
APilarca <angelpill15@gmail.com>
2017-02-16 22:48:55 -0800

    create more error handling

--- app1.js
+++ app1.js
@@ -21,8 +21,12 @@ function getProfile(username) {
 			});
 
 			response.on('end', () => {
-				const profile = JSON.parse(body);
-				printMessage(username, profile.badges.length, profile.points.JavaScript);
+				try {
+					const profile = JSON.parse(body);
+					printMessage(username, profile.badges.length, profile.points.JavaScript);
+				} catch(error) {
+					console.error(error.message);
+				}
 			});
 		});
 		request.on('error', error => console.error(`Problem with request: ${error.message}`))
